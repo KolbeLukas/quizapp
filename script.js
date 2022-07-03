01dbc72d8b80dc18f82710334d5efbe740191f5c
Lukas Kolbe <kolbe.lukas@gmx.de>
2022-07-03 11:00:00 +0200

    JSON-Array added

---
+++ script.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+        'question': 'Wie sehen HTML-Tags aus?',
+        'answer_1': '>p<>/p<',
+        'answer_2': '<p></p>',
+        'answer_3': '<<p>><</p>>',
+        'answer_4': '<p Hallo mein Text/p>',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wofür steht HTML?',
+        'answer_1': 'Hier Texten Mathematiker Leidenschaftlich',
+        'answer_2': 'Heizung Thermal Mechaniker Language',
+        'answer_3': 'Hunde Tiere Miezen Löwen',
+        'answer_4': 'Hyper Text Markup Language',
+        'right_answer': 4
+    },
+    {
+        'question': 'Kann man Listen in HTML einfügen?',
+        'answer_1': '<50>',
+        'answer_2': 'Nein',
+        'answer_3': 'Ja, mit <ul>',
+        'answer_4': '<p>',
+        'right_answer': 3
+    },
+    {
+        'question': 'Wie fügt man Bilder ein?',
+        'answer_1': '<img>link>text</img>',
+        'answer_2': '<img> src="link" alt="text"</img>',
+        'answer_3': '<img src="link" alt="text">',
+        'answer_4': 'strg + V',
+        'right_answer': 3
+    },
+    {
+        'question': 'Wie fügt man einen Zeilenumbruch ein?',
+        'answer_1': '<br>',
+        'answer_2': '<->',
+        'answer_3': '<brrrrrrrr>',
+        'answer_4': '<>',
+        'right_answer': 1
+    },
+    {
+        'question': 'Was bewirkt <meta charset="utf-8">?',
+        'answer_1': 'Der Titel der Webseite wird auf arabisch übersetzt',
+        'answer_2': 'ä ö ü werden erkannt und lesbar angezeigt',
+        'answer_3': 'Die Buchstaben der Tastatur werden vertauscht',
+        'answer_4': 'Die Webseite bekommt einen coolen Touch',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wie sieht die Struktur einer HTML-Seite aus?',
+        'answer_1': '<head> <body> </head> </body>',
+        'answer_2': '<html> <head></head> <body></body> </html>',
+        'answer_3': '<html> <body></body> <head></head> </html>',
+        'answer_4': '<body> <head></head> <main></main> </body>',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wieviele Überschrift-arten gibt es?',
+        'answer_1': 'h1 bis h6',
+        'answer_2': 'h1 bis h3',
+        'answer_3': 'h0 bis h5',
+        'answer_4': 'h2 bis h8',
+        'right_answer': 1
+    },
+    {
+        'question': 'Wie bezeichnet man das: <div>?',
+        'answer_1': 'Als Abend',
+        'answer_2': 'Als Morgen',
+        'answer_3': 'Als Nacht',
+        'answer_4': 'Als Tag',
+        'right_answer': 4
+    },
+    {
+        'question': 'Mit welchem HTML-Element wird ein Text kursiv geschrieben?',
+        'answer_1': '<i>',
+        'answer_2': '<p>',
+        'answer_3': '<u>',
+        'answer_4': '<k>',
+        'right_answer': 1
+    }
+];
