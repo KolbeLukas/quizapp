a413e16cfb292a87e754198804879fc4f6f48962
Lukas Kolbe <kolbe.lukas@gmx.de>
2022-07-04 17:56:30 +0200

    added score window

--- script.js
+++ script.js
@@ -19,23 +19,23 @@ let HTMLQuiz = [
         'question': 'Kann man Listen in HTML einfügen?',
         'answer-1': '<50>',
         'answer-2': 'Nein',
-        'answer-3': 'Ja, mit <ul>',
-        'answer-4': '<p>',
+        'answer-3': 'Ja, mit &lt;ul&gt;',
+        'answer-4': '&lt;p&gt;',
         'right-answer': 3
     },
     {
         'question': 'Wie fügt man Bilder ein?',
-        'answer-1': '<img>link>text</img>',
-        'answer-2': '<img> src="link" alt="text"</img>',
-        'answer-3': '<img src="link" alt="text">',
+        'answer-1': '&lt;img&gt;link>text&lt;/img&gt;',
+        'answer-2': '&lt;img&gt; src="link" alt="text"&lt;/img&gt;',
+        'answer-3': '&lt;img src="link" alt="text"&gt;',
         'answer-4': 'strg + V',
         'right-answer': 3
     },
     {
         'question': 'Wie fügt man einen Zeilenumbruch ein?',
-        'answer-1': '<br>',
+        'answer-1': '&lt;br&gt;',
         'answer-2': '<->',
-        'answer-3': '<brrrrrrrr>',
+        'answer-3': '&lt;brrrrrrrr&gt;',
         'answer-4': '<>',
         'right-answer': 1
     },
@@ -49,10 +49,10 @@ let HTMLQuiz = [
     },
     {
         'question': 'Wie sieht die Struktur einer HTML-Seite aus?',
-        'answer-1': '<head> <body> </head> </body>',
-        'answer-2': '<html> <head></head> <body></body> </html>',
-        'answer-3': '<html> <body></body> <head></head> </html>',
-        'answer-4': '<body> <head></head> <main></main> </body>',
+        'answer-1': '&lt;head&gt; &lt;body&gt; &lt;/head&gt; &lt;/body&gt;',
+        'answer-2': '&lt;html&gt; &lt;head&gt;&lt;/head&gt; &lt;body&gt;&lt;/body&gt; &lt;/html&gt;',
+        'answer-3': '&lt;html&gt; &lt;body&gt;&lt;/body&gt; &lt;head&gt;&lt;/head&gt; &lt;/html&gt;',
+        'answer-4': '&lt;body&gt; &lt;head&gt;&lt;/head&gt; &lt;main&gt;&lt;/main&gt; &lt;/body&gt;',
         'right-answer': 2
     },
     {
@@ -73,10 +73,10 @@ let HTMLQuiz = [
     },
     {
         'question': 'Mit welchem HTML-Element wird ein Text kursiv geschrieben?',
-        'answer-1': '<i>',
-        'answer-2': '<p>',
-        'answer-3': '<u>',
-        'answer-4': '<k>',
+        'answer-1': '&lt;i&gt;',
+        'answer-2': '&lt;p&gt;',
+        'answer-3': '&lt;u&gt;',
+        'answer-4': '&lt;k&gt;',
         'right-answer': 1
     }
 ];
@@ -94,35 +94,45 @@ function loadHTMLQuizStart() {
 
 
 function startHTMLQuiz() {
+    let quizString = 'HTMLQuiz';
+    let quiz = HTMLQuiz;
+
     document.getElementById('quiz-content').classList.remove('main-bg');
-    loadQuestionAndAnswers(currentQuestion);
+    loadQuestionAndAnswers(quizString, quiz);
 }
 
 
-function loadQuestionAndAnswers(currentQuestion) {
-    let quiz = 'HTMLQuiz';
-    let question = HTMLQuiz[currentQuestion];
-
-    document.getElementById('quiz-content').innerHTML = renderQuestionAndAnswers(quiz, question);
-    document.getElementById('max-amount-questions').innerHTML = HTMLQuiz.length;
+function loadQuestionAndAnswers(quizString, quiz) {
+    if (currentQuestion >= HTMLQuiz.length) {
+        document.getElementById('quiz-content').innerHTML = renderScoreWindow();
+    } else {
+        let quizdata = quiz[currentQuestion];
+        document.getElementById('quiz-content').innerHTML = renderQuestionAndAnswers(quizString, quizdata);
+        document.getElementById('max-amount-questions').innerHTML = HTMLQuiz.length;
+        document.getElementById('current-question-number').innerHTML = currentQuestion + 1;
+    }
 }
 
 
-function answer(quiz, selection) {
-    eval(quiz);
-    let question = quiz[currentQuestion]
+function answer(quizString, selection) {
+    let question = quizString[currentQuestion];
     let selctedAnswerNumber = selection.slice(-1);
     let idOfRightAnswer = `answer-${question['right-answer']}`;
 
-    if (selctedAnswerNumber == question['right-answer']){
-        console.log('Richtig')
+    if (selctedAnswerNumber == question['right-answer']) {
         document.getElementById(selection).classList.add('bg-success');
     } else {
-        console.log('Falsch')
         document.getElementById(selection).classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).classList.add('bg-success');
     }
     document.getElementById('next-question').disabled = false;
+}
+
+
+function nextQuestion(quizString) {
+    currentQuestion++;
+    let quiz = eval(quizString);
+    loadQuestionAndAnswers(quizString, quiz);
 }
 
 
@@ -143,39 +153,53 @@ function renderHTMLQuizStart() {
 }
 
 
-function renderQuestionAndAnswers(quiz, question) {
+function renderQuestionAndAnswers(quizString, question) {
     return /*html*/`
         <div class="card-body d-flex flex-column justify-content-center align-items-center w-100">
             <h4 class="card-title text-center fw-medium rubik mb-4">${question['question']}</h4>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer-1" onclick="answer(${quiz}, 'answer-1')">
+                <div class="card-body p-2 d-flex align-items-center answer-hover" id="answer-1" onclick="answer(${quizString}, 'answer-1')">
                     <span class="badge bg-badge me-4">A</span>
                     <span class="rubik">${question['answer-1']}</span>
                 </div>
             </div>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer-2" onclick="answer(${quiz}, 'answer-2')">
+                <div class="card-body p-2 d-flex align-items-center answer-hover" id="answer-2" onclick="answer(${quizString}, 'answer-2')">
                     <span class="badge bg-badge me-4">B</span>
                     <span class="rubik">${question['answer-2']}</span>
                 </div>
             </div>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer-3" onclick="answer(${quiz}, 'answer-3')">
+                <div class="card-body p-2 d-flex align-items-center answer-hover" id="answer-3" onclick="answer(${quizString}, 'answer-3')">
                     <span class="badge bg-badge me-4">C</span>
                     <span class="rubik">${question['answer-3']}</span>
                 </div>
             </div>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer-4" onclick="answer(${quiz}, 'answer-4')">
+                <div class="card-body p-2 d-flex align-items-center answer-hover" id="answer-4" onclick="answer(${quizString}, 'answer-4')">
                     <span class="badge bg-badge me-4">D</span>
                     <span class="rubik">${question['answer-4']}</span>
                 </div>
             </div>
             <div class="d-flex justify-content-between align-items-center w-75">
-                <span><b>1</b> von <b id="max-amount-questions"></b> Fragen</span>
-                <button id="next-question" class="btn btn-primary ps-3 pe-3 d-flex align-items-center radius-left radius-right" disabled>
+                <span><b id="current-question-number"></b> von <b id="max-amount-questions"></b> Fragen</span>
+                <button id="next-question" onclick="nextQuestion('${quizString}')" class="btn btn-primary ps-3 pe-3 d-flex align-items-center radius-left radius-right" disabled>
                 <span>Nächste Frage</span>
                 </button>
             </div>
+        </div>`;
+}
+
+
+function renderScoreWindow() {
+    return /*html*/`
+        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+            <img src="img/brain-result.png" class="w-50 mb-4">
+            <h4 class="card-title text-center fw-bold rubik w-50 mb-4">COMPLETE HTML QUIZ</h4>
+            <p class="card-text fw-bold fs-5 rubik d-flex justify-content-between w-75">
+                <span class="score-color">DEIN SCORE </span>
+                <span>10/10</span>
+            </p>
+            <p class="card-text rubik">Wähle links ein weiters Quiz aus.</p>
         </div>`;
 }
