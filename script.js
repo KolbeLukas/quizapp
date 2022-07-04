2436704cb1cfae69ccf3d8cf0d0622f921789d6e
Lukas Kolbe <kolbe.lukas@gmx.de>
2022-07-04 11:57:25 +0200

    next question

--- script.js
+++ script.js
@@ -1,83 +1,83 @@
 let HTMLQuiz = [
     {
         'question': 'Wie sehen HTML-Tags aus?',
-        'answer_1': '>p<>/p<',
-        'answer_2': '&lt;p&gt;&lt;/p&gt;',
-        'answer_3': '<&lt;p&gt;><&lt;/p&gt;>',
-        'answer_4': '&lt;p Hallo mein Text/p&gt;',
-        'right_answer': 2
+        'answer-1': '>p<>/p<',
+        'answer-2': '&lt;p&gt;&lt;/p&gt;',
+        'answer-3': '<&lt;p&gt;><&lt;/p&gt;>',
+        'answer-4': '&lt;p Hallo mein Text/p&gt;',
+        'right-answer': 2
     },
     {
         'question': 'Wofür steht HTML?',
-        'answer_1': 'Hier Texten Mathematiker Leidenschaftlich',
-        'answer_2': 'Heizung Thermal Mechaniker Language',
-        'answer_3': 'Hunde Tiere Miezen Löwen',
-        'answer_4': 'Hyper Text Markup Language',
-        'right_answer': 4
+        'answer-1': 'Hier Texten Mathematiker Leidenschaftlich',
+        'answer-2': 'Heizung Thermal Mechaniker Language',
+        'answer-3': 'Hunde Tiere Miezen Löwen',
+        'answer-4': 'Hyper Text Markup Language',
+        'right-answer': 4
     },
     {
         'question': 'Kann man Listen in HTML einfügen?',
-        'answer_1': '<50>',
-        'answer_2': 'Nein',
-        'answer_3': 'Ja, mit <ul>',
-        'answer_4': '<p>',
-        'right_answer': 3
+        'answer-1': '<50>',
+        'answer-2': 'Nein',
+        'answer-3': 'Ja, mit <ul>',
+        'answer-4': '<p>',
+        'right-answer': 3
     },
     {
         'question': 'Wie fügt man Bilder ein?',
-        'answer_1': '<img>link>text</img>',
-        'answer_2': '<img> src="link" alt="text"</img>',
-        'answer_3': '<img src="link" alt="text">',
-        'answer_4': 'strg + V',
-        'right_answer': 3
+        'answer-1': '<img>link>text</img>',
+        'answer-2': '<img> src="link" alt="text"</img>',
+        'answer-3': '<img src="link" alt="text">',
+        'answer-4': 'strg + V',
+        'right-answer': 3
     },
     {
         'question': 'Wie fügt man einen Zeilenumbruch ein?',
-        'answer_1': '<br>',
-        'answer_2': '<->',
-        'answer_3': '<brrrrrrrr>',
-        'answer_4': '<>',
-        'right_answer': 1
+        'answer-1': '<br>',
+        'answer-2': '<->',
+        'answer-3': '<brrrrrrrr>',
+        'answer-4': '<>',
+        'right-answer': 1
     },
     {
         'question': 'Was bewirkt <meta charset="utf-8">?',
-        'answer_1': 'Der Titel der Webseite wird auf arabisch übersetzt',
-        'answer_2': 'ä ö ü werden erkannt und lesbar angezeigt',
-        'answer_3': 'Die Buchstaben der Tastatur werden vertauscht',
-        'answer_4': 'Die Webseite bekommt einen coolen Touch',
-        'right_answer': 2
+        'answer-1': 'Der Titel der Webseite wird auf arabisch übersetzt',
+        'answer-2': 'ä ö ü werden erkannt und lesbar angezeigt',
+        'answer-3': 'Die Buchstaben der Tastatur werden vertauscht',
+        'answer-4': 'Die Webseite bekommt einen coolen Touch',
+        'right-answer': 2
     },
     {
         'question': 'Wie sieht die Struktur einer HTML-Seite aus?',
-        'answer_1': '<head> <body> </head> </body>',
-        'answer_2': '<html> <head></head> <body></body> </html>',
-        'answer_3': '<html> <body></body> <head></head> </html>',
-        'answer_4': '<body> <head></head> <main></main> </body>',
-        'right_answer': 2
+        'answer-1': '<head> <body> </head> </body>',
+        'answer-2': '<html> <head></head> <body></body> </html>',
+        'answer-3': '<html> <body></body> <head></head> </html>',
+        'answer-4': '<body> <head></head> <main></main> </body>',
+        'right-answer': 2
     },
     {
         'question': 'Wieviele Überschrift-arten gibt es?',
-        'answer_1': 'h1 bis h6',
-        'answer_2': 'h1 bis h3',
-        'answer_3': 'h0 bis h5',
-        'answer_4': 'h2 bis h8',
-        'right_answer': 1
+        'answer-1': 'h1 bis h6',
+        'answer-2': 'h1 bis h3',
+        'answer-3': 'h0 bis h5',
+        'answer-4': 'h2 bis h8',
+        'right-answer': 1
     },
     {
         'question': 'Wie bezeichnet man das: <div>?',
-        'answer_1': 'Als Abend',
-        'answer_2': 'Als Morgen',
-        'answer_3': 'Als Nacht',
-        'answer_4': 'Als Tag',
-        'right_answer': 4
+        'answer-1': 'Als Abend',
+        'answer-2': 'Als Morgen',
+        'answer-3': 'Als Nacht',
+        'answer-4': 'Als Tag',
+        'right-answer': 4
     },
     {
         'question': 'Mit welchem HTML-Element wird ein Text kursiv geschrieben?',
-        'answer_1': '<i>',
-        'answer_2': '<p>',
-        'answer_3': '<u>',
-        'answer_4': '<k>',
-        'right_answer': 1
+        'answer-1': '<i>',
+        'answer-2': '<p>',
+        'answer-3': '<u>',
+        'answer-4': '<k>',
+        'right-answer': 1
     }
 ];
 
@@ -111,18 +111,18 @@ function loadQuestionAndAnswers(currentQuestion) {
 function answer(quiz, selection) {
     eval(quiz);
     let question = quiz[currentQuestion]
-    console.log('answer', selection);
     let selctedAnswerNumber = selection.slice(-1);
-    console.log('richtig ist', question['right_answer']);
-    console.log('Antwort ausgewählt', selctedAnswerNumber);
+    let idOfRightAnswer = `answer-${question['right-answer']}`;
 
-    if (selctedAnswerNumber == question['right_answer']){
+    if (selctedAnswerNumber == question['right-answer']){
         console.log('Richtig')
         document.getElementById(selection).classList.add('bg-success');
     } else {
         console.log('Falsch')
         document.getElementById(selection).classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).classList.add('bg-success');
     }
+    document.getElementById('next-question').disabled = false;
 }
 
 
@@ -148,32 +148,32 @@ function renderQuestionAndAnswers(quiz, question) {
         <div class="card-body d-flex flex-column justify-content-center align-items-center w-100">
             <h4 class="card-title text-center fw-medium rubik mb-4">${question['question']}</h4>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer_1" onclick="answer(${quiz}, 'answer_1')">
+                <div class="card-body p-2 answer-hover" id="answer-1" onclick="answer(${quiz}, 'answer-1')">
                     <span class="badge bg-badge me-4">A</span>
-                    <span class="rubik">${question['answer_1']}</span>
+                    <span class="rubik">${question['answer-1']}</span>
                 </div>
             </div>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer_2" onclick="answer(${quiz}, 'answer_2')">
+                <div class="card-body p-2 answer-hover" id="answer-2" onclick="answer(${quiz}, 'answer-2')">
                     <span class="badge bg-badge me-4">B</span>
-                    <span class="rubik">${question['answer_2']}</span>
+                    <span class="rubik">${question['answer-2']}</span>
                 </div>
             </div>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer_3" onclick="answer(${quiz}, 'answer_3')">
+                <div class="card-body p-2 answer-hover" id="answer-3" onclick="answer(${quiz}, 'answer-3')">
                     <span class="badge bg-badge me-4">C</span>
-                    <span class="rubik">${question['answer_3']}</span>
+                    <span class="rubik">${question['answer-3']}</span>
                 </div>
             </div>
             <div class="card mb-3 w-75">
-                <div class="card-body p-2 answer-hover" id="answer_4" onclick="answer(${quiz}, 'answer_4')">
+                <div class="card-body p-2 answer-hover" id="answer-4" onclick="answer(${quiz}, 'answer-4')">
                     <span class="badge bg-badge me-4">D</span>
-                    <span class="rubik">${question['answer_4']}</span>
+                    <span class="rubik">${question['answer-4']}</span>
                 </div>
             </div>
             <div class="d-flex justify-content-between align-items-center w-75">
                 <span><b>1</b> von <b id="max-amount-questions"></b> Fragen</span>
-                <button class="btn btn-primary ps-3 pe-3 d-flex align-items-center radius-left radius-right">
+                <button id="next-question" class="btn btn-primary ps-3 pe-3 d-flex align-items-center radius-left radius-right" disabled>
                 <span>Nächste Frage</span>
                 </button>
             </div>
